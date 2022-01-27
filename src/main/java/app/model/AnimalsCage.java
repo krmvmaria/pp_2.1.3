094dc5cd57b74cde0f3aa20bc7a749658aea1dbe
krmvmaria <krmvmaria@mail.ru>
2022-01-27 12:21:00 +0300

    first commit

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,19 +1,30 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
+    private final Animal animal1;
+    private final Animal animal2;
+    private final Timer timer;
 
     @Autowired
-    private Animal animal;
+    public AnimalsCage(@Qualifier("dog")Animal animal1, @Qualifier("cat") Animal animal2, @Qualifier("timer") Timer timer) {
+        this.animal1 = animal1;
+        this.animal2 = animal2;
+        this.timer = timer;
+    }
 
     public void whatAnimalSay() {
         System.out.println("Say:");
-        System.out.println(animal.toString());
+        System.out.println(animal1.toString() + ", " + animal2.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(timer.getTime());
         System.out.println("________________________");
+    }
+    public Timer getTimer(){
+        return timer;
     }
 }
